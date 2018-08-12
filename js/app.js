790f396a305f4449bf9d745ebca6968dbb20b970
pimo333 <melodysong88@gmail.com>
2018-08-12 01:26:23 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var main = function() {
+    $('.nav li').click(function() {
+        var category = $(this).attr('class');
+
+        $('.nav li').removeClass('active');
+        $(this).addClass('active');
+
+        if (category === "nav-consumer") {
+            $('.thumbnail').removeClass('selected');
+            $('.consumer').addClass('selected');
+            A
+        } else if (category === "nav-mobile") {
+            $('.thumbnail').removeClass('selected');
+            $('.mobile').addClass('selected');
+        } else if (category === "nav-commerce") {
+            $('.thumbnail').removeClass('selected');
+            $('.commerce').addClass('selected');
+        } else if (category === "nav-enterprise") {
+            $('.thumbnail').removeClass('selected');
+            $('.enterprise').addClass('selected');
+        } else {
+            $('.thumbnail').removeClass('selected');
+        }
+    });
+};
+$(document).ready(main);
